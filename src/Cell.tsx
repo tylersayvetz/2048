11326a79d0d125f5b558fcc11d8df655ee1ab216
Tyler <tylersayvetz@gmail.com>
2020-02-08 15:07:07 -0800

    fix aesthetic bugs/ add icons

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -7,12 +7,24 @@ interface CellProps {
 }
 
 export default function Cell(props: CellProps) {
-  // const color: string = 'hsl(22,100%,' + (props.contents !== null ? 100 - (100 / (props.contents! * 10)) : 100) + '%';
-  const colors: Array<string> = ['#E1B07E', '#E5C19E', 'rgb(245, 197, 109)', '#A5998C', '#937F6A', 'indigo', 'violet', 'turquiose', 'lightyellow', 'brown', 'black'];
+  const colors: Array<string> = [
+    '#E1B07E',   
+    '#E5C19E', 
+    'rgb(245, 197, 109)', 
+    '#A5998C', 
+    '#A1867F', 
+    '#9B829A', 
+    '#C49BBB', 
+    '#D1BCE3', 
+    '#AF5048', 
+    '#f2a916', 
+    '#ffee30'
+  ];
   const index = Math.log2(props.contents || -1);
   const style: React.CSSProperties = {
     backgroundColor: colors[index - 1],
-    color: index < 4 ? 'rgb(83, 80, 58)' : 'ivory',
+    color: (index < 4 || index === 9) ? 'rgb(83, 80, 58)' : 'ivory',
+    fontSize: index >= 10 ? '40px' : '50px'
   }
 
   return (
