8d455b767048e4337448669c1127bdd9a635c551
Tyler <tylersayvetz@gmail.com>
2020-05-26 15:50:06 -0700

    add links

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
       <header className="App-header">
         <h1>Use the Arrow Keys to Play!</h1>
         <Twenty48/>
+        <a id="github-link" href="https://github.com/tylersayvetz/2048">GitHub</a>
+        <a href="http://tylersayvetz.com">Creator</a>
       </header>
     </div>
   );
